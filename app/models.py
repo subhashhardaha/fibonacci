1bcce25c7956b2cabab79696be7a13449942251c
Subhash Hardaha <subhashhardaha@Subhashs-MacBook-Pro.local>
2019-03-14 02:24:38 +0530

    Added data model to store the fibinonacci nth term

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Fibonacci(models.Model):
+    number = models.TextField()
+    fib_value = models.TextField()
+
+    def __str__(self):
+        return self.number
