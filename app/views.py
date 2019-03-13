b64e0af9eda859ef6dcca62a661b37e7ec375812
Subhash Hardaha <subhashhardaha@Subhashs-MacBook-Pro.local>
2019-03-14 02:47:55 +0530

    Changed views to class based view and form action to post

--- app/views.py
+++ app/views.py
@@ -1,20 +1,32 @@
 from django.shortcuts import render
+from django.views.generic.base import View
 
 from .models import Fibonacci
 from .utils import fibonacci_nth
 
+
 # Create your views here.
-def home(request):
-    context = {}
-    n = request.GET.get('fibonacci_number', None)
-    if n:
-        if Fibonacci.objects.filter(number=n).exists():
-            obj = Fibonacci.objects.get(number=n)
-            value = obj.fib_value
-            context['fibonacci_value'] = value
-        else:
-            ans = fibonacci_nth(int(n))
-            context['fibonacci_value'] = ans
-            data = Fibonacci(number=n, fib_value=ans)
-            data.save()
-    return render(request, 'app/index.html', context)
+
+class Home(View):
+    template_name = 'app/index.html'
+
+    def get(self, request, *args, **kwargs):
+        context={}
+        print("get")
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        context = {}
+        print("post")
+        n=request.POST['fibonacci_number']
+        if n:
+            if Fibonacci.objects.filter(number=n).exists():
+                obj = Fibonacci.objects.get(number=n)
+                value = obj.fib_value
+                context['fibonacci_value'] = value
+            else:
+                ans = fibonacci_nth(int(n))
+                context['fibonacci_value'] = ans
+                data = Fibonacci(number=n, fib_value=ans)
+                data.save()
+        return render(request, self.template_name, context)
