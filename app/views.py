b8bc3fc18aca310967a2b5db39e20ec06864d19f
Subhash Hardaha <subhashhardaha@Subhashs-MacBook-Pro.local>
2019-03-14 11:57:16 +0530

    Added the matrix multiplication call in view

--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.views.generic.base import View
 
 from .models import Fibonacci
-from .utils import fibonacci_nth
+from .utils import fibonacci_nth, MatrixFibonacci
 from datetime import timedelta
 
 # Create your views here.
@@ -14,14 +14,12 @@ class Home(View):
 
     def get(self, request, *args, **kwargs):
         context={}
-        print("get")
         return render(request, self.template_name, context)
 
     def post(self, request, *args, **kwargs):
         start = time.time()
 
         context = {}
-        print("post")
         n=request.POST['fibonacci_number']
 
         if n:
@@ -31,12 +29,23 @@ class Home(View):
                 obj = Fibonacci.objects.get(number=n)
                 value = obj.fib_value
                 context['fibonacci_value'] = value
+                #time_taken_direct='0'
+                time_taken_mat='0'
             else:
-                ans = fibonacci_nth(int(n))
+                # start_d = time.time()
+                # fibonacci_nth(int(n))
+                # time_taken_direct=str(timedelta(seconds=time.time()-start_d))
+                start_m = time.time()
+                mf=MatrixFibonacci()
+                ans=mf.get_number(int(n))
+                time_taken_mat = str(timedelta(seconds=time.time() - start_m))
                 context['fibonacci_value'] = ans
                 data = Fibonacci(number=n, fib_value=ans)
                 data.save()
         end = time.time()
         time_taken=str(timedelta(seconds=end-start))
-        context['time_taken']=time_taken
+        context['total_time_taken']=time_taken
+        #context['time_taken_direct'] = time_taken_direct
+        context['time_taken_mat'] = time_taken_mat
+
         return render(request, self.template_name, context)
