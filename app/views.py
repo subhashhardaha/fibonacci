e15ac327f21347c3e9593fac573c6933170aa3ec
Subhash Hardaha <subhashhardaha@Subhashs-MacBook-Pro.local>
2019-03-14 10:52:09 +0530

    Added logic to show the elapsed time to get the result

--- app/views.py
+++ app/views.py
@@ -1,9 +1,11 @@
+import time
+
 from django.shortcuts import render
 from django.views.generic.base import View
 
 from .models import Fibonacci
 from .utils import fibonacci_nth
-
+from datetime import timedelta
 
 # Create your views here.
 
@@ -16,10 +18,15 @@ class Home(View):
         return render(request, self.template_name, context)
 
     def post(self, request, *args, **kwargs):
+        start = time.time()
+
         context = {}
         print("post")
         n=request.POST['fibonacci_number']
+
         if n:
+            context['fibonacci_number']=n
+
             if Fibonacci.objects.filter(number=n).exists():
                 obj = Fibonacci.objects.get(number=n)
                 value = obj.fib_value
@@ -29,4 +36,7 @@ class Home(View):
                 context['fibonacci_value'] = ans
                 data = Fibonacci(number=n, fib_value=ans)
                 data.save()
+        end = time.time()
+        time_taken=str(timedelta(seconds=end-start))
+        context['time_taken']=time_taken
         return render(request, self.template_name, context)
